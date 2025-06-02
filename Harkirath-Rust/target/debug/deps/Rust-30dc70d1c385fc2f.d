43fd1bced3aef374dbf933159b7380ecc7618e24
Agent-cat <2300031042@kluniversity.in>
2025-06-02 12:41:02 +0530

    files

--- Harkirath-Rust/target/debug/deps/Rust-30dc70d1c385fc2f.d
+++ Harkirath-Rust/target/debug/deps/Rust-30dc70d1c385fc2f.d
@@ -1,9 +1,9 @@
-/home/vishnu/Documents/Rust/target/debug/deps/libRust-30dc70d1c385fc2f.rmeta: src/main.rs Cargo.toml
+/home/vishnu/Documents/Rust/Harkirath-Rust/target/debug/deps/libRust-30dc70d1c385fc2f.rmeta: src/main.rs Cargo.toml
 
-/home/vishnu/Documents/Rust/target/debug/deps/Rust-30dc70d1c385fc2f.d: src/main.rs Cargo.toml
+/home/vishnu/Documents/Rust/Harkirath-Rust/target/debug/deps/Rust-30dc70d1c385fc2f.d: src/main.rs Cargo.toml
 
 src/main.rs:
 Cargo.toml:
 
-# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
 # env-dep:CLIPPY_CONF_DIR
