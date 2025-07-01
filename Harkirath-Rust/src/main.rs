0dbbbe2acde2cc14da4485f3fe27203ebf6526c6
Agent-cat <2300031042@kluniversity.in>
2025-07-01 23:34:51 +0530

    new cargo

--- Harkirath-Rust/src/main.rs
+++ Harkirath-Rust/src/main.rs
@@ -1,10 +1,3 @@
 fn main() {
-    let mut num = 10;
-    let x = &num as *const i32; // It is raw pointer
-    let y = &mut num as *mut i32; // It is raw pointer
-
-    unsafe {
-        println!("{}", *x);
-        println!("{}", *y);
-    }
+    let x: u32 = 12;
 }
